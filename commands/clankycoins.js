8c048fe08f1bf2d8236ad1ca2b6a7a267a52f32d
jibbodahibbo <jtwesterkamp@gmail.com>
2020-07-17 07:53:25 -0500

    asyn fumbles

--- commands/clankycoins.js
+++ commands/clankycoins.js
@@ -2,29 +2,30 @@
 module.exports = {
 	name: 'clankycoins',
 	description: 'Coins from clanky',
+	(async function(){
+		execute(message, args) {
 
-	execute(message, args) {
-async function(){
-		const splitArgs = commandArgs.split(' ');
-		const tagName = splitArgs.shift();
-		const tagDescription = splitArgs.join(' ');
+			const splitArgs = commandArgs.split(' ');
+			const tagName = splitArgs.shift();
+			const tagDescription = splitArgs.join(' ');
 
-		try {
-			// equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
-			const cc = await ClankyCoins.create({
-				user_id: message.author.id,
-				username: message.author.username,
-				tag:message.author.tag,
-				coins:0
-			});
-			return message.reply(`${cc.tag} added to the Clanky Coin Ledger`);
-		}
-		catch (e) {
-			if (e.name === 'SequelizeUniqueConstraintError') {
-				return message.reply('That person already exists.');
+			try {
+				// equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
+				const cc = await ClankyCoins.create({
+					user_id: message.author.id,
+					username: message.author.username,
+					tag:message.author.tag,
+					coins:0
+				});
+				return message.reply(`${cc.tag} added to the Clanky Coin Ledger`);
+				}
+			catch (e) {
+				if (e.name === 'SequelizeUniqueConstraintError') {
+					return message.reply('That person already exists.');
+					}
+				return message.reply('Something went wrong with adding a tag.');
 			}
-			return message.reply('Something went wrong with adding a tag.');
 		}
-}
 
-	},};
+	}
+,};
