ef36db94751f9de5a68c0351996332a7465913fd
jibbodahibbo <jtwesterkamp@gmail.com>
2020-07-17 11:00:54 -0500

    Type->type

--- dbInit.js
+++ dbInit.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(process.env.DATABASE,process.env.USER, process.e
 
 const ClankyCoins = sequelize.define('clankycoins', {
   	user_id: {
-      Type: Sequelize.STRING,
+      type: Sequelize.String,
       unique: true,
     },
   	username: Sequelize.STRING,
