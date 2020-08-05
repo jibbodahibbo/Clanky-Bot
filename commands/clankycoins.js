662d2b0aeaba491a273ad4bd970cb3fe1f0c80a3
jibbodahibbo <jtwesterkamp@gmail.com>
2020-08-05 16:47:40 -0500

    help option.

--- commands/clankycoins.js
+++ commands/clankycoins.js
@@ -33,25 +33,31 @@ module.exports = {
 			console.log(message)
 			// Add person to ledger by name.
 			if (args.length==1){
+					if (args[0] == 'help'){
+						return message.reply('!clankycoins @user : !clankycoins add @user 100 : !clankycoins remove @user 100');
+
+					}else if (message.mentions.users.first()){
 							try {
-								// equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
+								// Create clankycoin usser in ledger.
 								const cc = await ClankyCoins.create({
 									user_id: message.mentions.users.first().id,
 									username: message.mentions.users.first().username,
 									tag:message.mentions.users.first().tag,
 									coins:0
 								});
-						}
+							}
 							catch (e) {
 								if (e.name === 'SequelizeUniqueConstraintError') {
 									const user = await ClankyCoins.findOne({ where: { username: args[1] } });
 									return message.reply("You have "+ user.coins + " clanky coins.");
-									}
+								}
 									console.log(e);
-								return message.reply('Something went wrong with adding a user to the clanky coins ledger.');
+									return message.reply('Something went wrong with adding a user to the clanky coins ledger.');
 							}
-							return message.reply( message.mentions.users.first().tag + ' added to the Clanky Coin Ledger');
-						}
+					return message.reply( message.mentions.users.first().tag + ' added to the Clanky Coin Ledger');
+				}
+			}
+
 			if (args.length>2){
 					const user = await ClankyCoins.findOne({ where: { username:message.mentions.users.first().username } });
 					if(user){
