ae82c34ecc5fee630cc91b81a29716bf0c71d6f8
jibbodahibbo <jtwesterkamp@gmail.com>
2020-08-06 07:46:39 -0500

    ;

--- commands/schedules.js
+++ commands/schedules.js
@@ -36,7 +36,7 @@ module.exports = {
 									});
 									let response="";
 									for(let i=0; i<view_games.length;i++){
-										response+=view_games[i].away_role_id +' '+view_games[i].away_coach_id +' At '+view_games[i].home_role_id +' '+view_games[i].home_coach_id +'\n'+
+										response+=view_games[i].away_role_id +' '+view_games[i].away_coach_id +' At '+view_games[i].home_role_id +' '+view_games[i].home_coach_id +'\n';
 									}
 									return message.reply(response);
 							}else{
