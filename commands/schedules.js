a11ad986fa162706158caeb40ad1b61edb203f6c
jibbodahibbo <jtwesterkamp@gmail.com>
2020-08-07 14:44:32 -0500

    else fixing.

--- commands/schedules.js
+++ commands/schedules.js
@@ -25,10 +25,9 @@ module.exports = {
 
 					return message.reply('Added '+ss.league+ 'Game'+ ss.game_num + ss.away_role_id + ' at ' + ss.home_role_id);
 
-					if (args[0]=='delete' && args.length==2){
+					}else if (args[0]=='delete' && args.length==2){
 						const deletion = await Schedules.destroy({ where: { id: parseInt(args[1]) } });
 						return message.reply('Scheduled Item Deleted')
-					}
 
 					}else if (args[0]=='view'){
 								console.log("show schedule")
@@ -43,7 +42,7 @@ module.exports = {
 										if (view_games[i].game_complete){
 										response+=':white_check_mark:'
 										}
-										response+=view_games[i].away_role_id +' '+view_games[i].away_coach_id +' At '+view_games[i].home_role_id +' '+view_games[i].home_coach_id +':id-> '+ view_games[i].id +'\n';
+										response+=view_games[i].away_role_id +' '+view_games[i].away_coach_id +' At '+view_games[i].home_role_id +' '+view_games[i].home_coach_id +' :id-> '+ view_games[i].id +'\n';
 									}
 
 									return message.channel.send(response);
