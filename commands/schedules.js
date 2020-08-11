e33219d68840cd19744743f46afbb804f2c483fb
jibbodahibbo <jtwesterkamp@gmail.com>
2020-08-11 09:29:48 -0500

    k

--- commands/schedules.js
+++ commands/schedules.js
@@ -1,5 +1,22 @@
 const {Schedules} = require('../dbInit');
-const { Op } = require('sequelize')
+const { Op } = require('sequelize');
+
+function createScheduleItem(args){
+	const ss = await Schedules.create({
+		league:args[0],
+		game_num:args[1],
+		away_role_id:args[2],
+		away_coach_id:args[3],
+		home_role_id:args[4],
+		home_coach_id:args[5],
+		game_complete: false
+	});
+	console.log("sending confirmation message");
+
+	return(ss);
+}
+
+
 module.exports = {
 	name: 'schedules',
 	description: 'scheduling teams',
@@ -9,21 +26,9 @@ module.exports = {
 	if (message.member.roles.cache.find(role => role.name == 'Commissioner') || message.member.roles.cache.find(role => role.name == 'Codehead')) {
 		console.log("Is Commisioner")
 					try {
-						// equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
 						if (args.length == 6){
-						const ss = await Schedules.create({
-		        	league:args[0],
-							game_num:args[1],
-		    			away_role_id:args[2],
-		    			away_coach_id:args[3],
-							home_role_id:args[4],
-							home_coach_id:args[5],
-							game_complete: false
-						});
-
-					console.log("sending confirmation message");
-
-					return message.reply('Added '+ss.league+ ' Game '+ ss.game_num + ss.away_role_id + ' at ' + ss.home_role_id);
+							scheduled_item = createScheduleItem(args);
+							return message.reply('Added '+scheduled_item.league+ ' Game '+ scheduled_item.game_num + scheduled_item.away_role_id + ' at ' + scheduled_item.home_role_id);
 
 					}else if (args[0]=='delete' && args.length==2){
 						const deletion = await Schedules.destroy({ where: { id: parseInt(args[1]) } });
@@ -50,23 +55,23 @@ module.exports = {
 										return message.channel.send(response);
 								}else{
 									 // Shows the schedule for Schedule for one game_number in a league.
-								const view_games = await Schedules.findAll({
-									  where: {
-									    league: args[1],
-											game_num: args[2]
-									  }
-									});
-									let response="";
-									for(let i=0; i<view_games.length;i++){
-										if (view_games[i].game_complete){
-										response+=':white_check_mark:'
-									}else{
-										response+=':o:'
-									}
-										response+=view_games[i].away_role_id +' '+view_games[i].away_coach_id +' At '+view_games[i].home_role_id +' '+view_games[i].home_coach_id +' :id-> '+ view_games[i].id +'\n';
-									}
-								}
+									const view_games = await Schedules.findAll({
+										  where: {
+										    league: args[1],
+												game_num: args[2]
+										  }
+										});
+										let response="";
+										for(let i=0; i<view_games.length;i++){
+											if (view_games[i].game_complete){
+											response+=':white_check_mark:'
+										}else{
+											response+=':o:'
+										}
+											response+=view_games[i].away_role_id +' '+view_games[i].away_coach_id +' At '+view_games[i].home_role_id +' '+view_games[i].home_coach_id +' :id-> '+ view_games[i].id +'\n';
+										}
 									return message.channel.send(response);
+									}
 							}else{
 								console.log("sending Not confirmation message");
 								return message.reply('Please try the format of !schedules [LEAGUE] [GAME#] [AWAY TEAM ROLE] [AWAY TEAM INITAL] [HOME TEAM ROLE] [HOME TEAM INITIAL]');
