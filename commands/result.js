7a8b78b6dfb85e173b76f8a34458f0f3726f0409
John Lund <johnrlund0@gmail.com>
2020-08-12 21:32:08 -0600

    Wrapped some responses in embeds

--- commands/result.js
+++ commands/result.js
@@ -1,5 +1,5 @@
-const { Op } = require('sequelize')
-const {Schedules, Results} = require('../dbInit');
+const { Op } = require("sequelize");
+const { Schedules, Results } = require("../dbInit");
 const discord = require("../byb-bot.js").discord;
 /**
  *
@@ -23,7 +23,7 @@ const discord = require("../byb-bot.js").discord;
  *  }
  */
 async function getResultPair(args, test_response = false) {
-    if (test_response) {
+	if (test_response) {
 		return {
 			league: "lulu",
 			coach: "BB",
@@ -33,39 +33,47 @@ async function getResultPair(args, test_response = false) {
 			],
 		};
 	}
-  
-    const sched = await Schedules.findOne(
-    {where:{
-      league: args.league ,
-      game_num:args.game_num,
-      [Op.or]: [{ away_coach_id: args.coach }, { home_coach_id: args.coach }]
-      }
-    });
 
-    let opponent ="";
-    if (sched.away_coach_id == args.coach){
-      opponent=sched.home_coach_id;
-    }else{
-      opponent=sched.away_coach_id;
-    }
+	const sched = await Schedules.findOne({
+		where: {
+			league: args.league,
+			game_num: args.game_num,
+			[Op.or]: [
+				{ away_coach_id: args.coach },
+				{ home_coach_id: args.coach },
+			],
+		},
+	});
 
+	let opponent = "";
+	if (sched.away_coach_id == args.coach) {
+		opponent = sched.home_coach_id;
+	} else {
+		opponent = sched.away_coach_id;
+	}
 
-  const other_result = await Results.findOne(
-    {where:{
-    league: args.league ,
-    game_num:args.game_num,
-    coach: opponent,
-      }
-    });
+	const other_result = await Results.findOne({
+		where: {
+			league: args.league,
+			game_num: args.game_num,
+			coach: opponent,
+		},
+	});
 
-  const complete_game = await Schedules.update({ game_complete: true },
-    { where:{
-      league: args.league ,
-      game_num:args.game_num,
-      [Op.or]: [{ away_coach_id: args.coach }, { home_coach_id: args.coach }]
-      }
-    });
-  return other_result;
+	const complete_game = await Schedules.update(
+		{ game_complete: true },
+		{
+			where: {
+				league: args.league,
+				game_num: args.game_num,
+				[Op.or]: [
+					{ away_coach_id: args.coach },
+					{ home_coach_id: args.coach },
+				],
+			},
+		}
+	);
+	return other_result;
 }
 
 /**
@@ -82,19 +90,17 @@ async function getResultPair(args, test_response = false) {
  * @description saves the current result to the database
  * @returns void (maybe true or false for success/failure to write to the db)
  */
-async function saveResult(result, testing=false) {
-    if (testing) {
-        return;
-    }  
-    else {
-        const r = await Results.create({
+async function saveResult(result, testing = false) {
+	if (testing) {
+		return;
+	} else {
+		const r = await Results.create({
 			league: result.league,
 			coach: result.coach,
 			game_num: result.game_num,
 			images: result.images,
 		});
-    }
-    
+	}
 }
 
 /**
@@ -119,157 +125,169 @@ async function saveResult(result, testing=false) {
  *  }
  */
 async function getScheduleData(args, test_response = false) {
-  if (test_response) {
+	if (test_response) {
 		return {
 			away_coach_id: "IN",
 			away_role_id: "<@&735310865284333651>", // TODO: change to valid role id for this server
 			home_coach_id: "BB",
 			home_role_id: "<@&735310832090742865>", // TODO: change to valid role id for this server
 		};
-  }
-  
-    const sched = await Schedules.findOne(
-    {where:{
-    league: args.league ,
-    game_num:args.game_num,
-    [Op.or]: [{ away_coach_id: args.coach }, { home_coach_id: args.coach }]
-    }
-    });
-    
-    
-    return {
-        away_coach_id: sched.away_coach_id,
-        home_coach_id: sched.home_coach_id,
-        away_role_id: sched.away_role_id,
-        home_role_id: sched.home_role_id,
-        };
-        
+	}
+
+	const sched = await Schedules.findOne({
+		where: {
+			league: args.league,
+			game_num: args.game_num,
+			[Op.or]: [
+				{ away_coach_id: args.coach },
+				{ home_coach_id: args.coach },
+			],
+		},
+	});
+
+	return {
+		away_coach_id: sched.away_coach_id,
+		home_coach_id: sched.home_coach_id,
+		away_role_id: sched.away_role_id,
+		home_role_id: sched.home_role_id,
+	};
 }
 
 let results_channel_id = "733773776357163079"; // TODO: change to valid channel for this server
 
 module.exports = {
-    name: 'result',
-    description: 'Result',
-    async execute(message, args, client) {
-        let coach_regex = RegExp("([A-Z][A-Z])");
-        let coach = "";
-        let league_list = ["lulu", "paste"];
-        let league_list_string = league_list.join("|")
-        let league_regex = RegExp(`(${league_list_string})`, "i");
-        let league = "";
-        let game_num_regex = RegExp("G([0-9]+)", "i");
-        let game_num = "";
-        let game_score_regex = RegExp("([0-9]+)-([0-9]+)");
-        let player_score = "";
-        let computer_score = "";
-        let images = [];
-        const enforcing_score = false;
-        const example_command = "`!result [lulu or paste] [coach initials] G[game number] [your score] - [their score]`";
-        const color_unrecorded = 13632027;
-        const color_recorded = 4289797;
-        const color_pending = 16312092;
+	name: "result",
+	description: "Result",
+	async execute(message, args, client) {
+		let coach_regex = RegExp("([A-Z][A-Z])");
+		let coach = "";
+		let league_list = ["lulu", "paste"];
+		let league_list_string = league_list.join("|");
+		let league_regex = RegExp(`(${league_list_string})`, "i");
+		let league = "";
+		let game_num_regex = RegExp("G([0-9]+)", "i");
+		let game_num = "";
+		let game_score_regex = RegExp("([0-9]+)-([0-9]+)");
+		let player_score = "";
+		let computer_score = "";
+		let images = [];
+		const enforcing_score = false;
+		const example_command =
+			"`!result [lulu or paste] [coach initials] G[game number] [your score] - [their score]`";
+		const color_unrecorded = 13632027;
+		const color_recorded = 4289797;
+		const color_pending = 16312092;
 
-        // make sure this is a dm. If not, let the user know they need to send a dm
-        if (message.guild !== null) {
+		// make sure this is a dm. If not, let the user know they need to send a dm
+		if (message.guild !== null) {
 			message.reply(
 				"`!result` commands can only be sent directly to this bot"
-            );
-            client.users.cache
+			);
+			client.users.cache
 				.get(message.author.id)
 				.send("You can send `!result` commands to me here!");
 			return;
 		}
 
-        // extract coach
-        if (coach_regex.test(message.content)) {
-           coach = message.content.match(coach_regex);
-            coach = args[1];
-       } else {
-           message.reply(
-                "Your result is missing a coach! Your message should look like this:"
-            );
-            message.reply(example_command);
-            return;
-        }
-        // extract league
-        if (league_regex.test(message.content)) {
-            league = message.content.match(league_regex);
-            league = league[1];
-        } else {
-            message.reply(
-                "Your result is missing a league! Your message should look like this:"
-            );
-            message.reply(example_command);
-            return;
-        }
-        // extract game number
-        if (game_num_regex.test(message.content)) {
-            game_num = message.content.match(game_num_regex);
-            game_num = game_num[1];
-        } else {
-            message.reply(
-                "Your result is missing a game number! Your message should look like this:"
-            );
-            message.reply(example_command);
-            return;
-        }
+		// extract coach
+		if (coach_regex.test(message.content)) {
+			coach = message.content.match(coach_regex);
+			coach = args[1];
+		} else {
+			message.reply(
+				"Your result is missing a coach! Your message should look like this:"
+			);
+			message.reply(example_command);
+			return;
+		}
+		// extract league
+		if (league_regex.test(message.content)) {
+			league = message.content.match(league_regex);
+			league = league[1];
+		} else {
+			message.reply(
+				"Your result is missing a league! Your message should look like this:"
+			);
+			message.reply(example_command);
+			return;
+		}
+		// extract game number
+		if (game_num_regex.test(message.content)) {
+			game_num = message.content.match(game_num_regex);
+			game_num = game_num[1];
+		} else {
+			message.reply(
+				"Your result is missing a game number! Your message should look like this:"
+			);
+			message.reply(example_command);
+			return;
+		}
 
-        // extract score
-        if (game_score_regex.test(message.content)) {
-            let game_score_result = message.content.match(game_score_regex);
-            player_score = game_score_result[1];
-            computer_score = game_score_result[2];
-        } else {
-            if (enforcing_score) {
-                message.reply(
+		// extract score
+		if (game_score_regex.test(message.content)) {
+			let game_score_result = message.content.match(game_score_regex);
+			player_score = game_score_result[1];
+			computer_score = game_score_result[2];
+		} else {
+			if (enforcing_score) {
+				message.reply(
 					"Your result is missing a score! Your message should look like this:"
 				);
 				message.reply(example_command);
 				return;
-            }
-        }
+			}
+		}
 
-        // extract images
-        if (message.attachments.size > 0) {
-            for (attachment of message.attachments.values()) {
-                images.push(attachment.attachment);
-            }
-        } else {
-            await message.reply("Your result still needs a screenshot! Reply with one to add it.");
-            const authorFilter = response => {
-                return response.author.id === message.author.id && response.attachments.size > 0;
-            };
-            let screenshotResponses;
-            try {
-                screenshotResponses = await message.channel.awaitMessages(authorFilter, { max: 1, time: 300000, errors: ['time'] });
-                images.push(screenshotResponses.first().attachments.first().attachment);
+		// extract images
+		if (message.attachments.size > 0) {
+			for (attachment of message.attachments.values()) {
+				images.push(attachment.attachment);
+			}
+		} else {
+			await message.reply(
+				"Your result still needs a screenshot! Reply with one to add it."
+			);
+			const authorFilter = (response) => {
+				return (
+					response.author.id === message.author.id &&
+					response.attachments.size > 0
+				);
+			};
+			let screenshotResponses;
+			try {
+				screenshotResponses = await message.channel.awaitMessages(
+					authorFilter,
+					{ max: 1, time: 300000, errors: ["time"] }
+				);
+				images.push(
+					screenshotResponses.first().attachments.first().attachment
+				);
+			} catch (errorScreenshotResponses) {
+				message.reply(
+					"I didn't get a message with a screenshot. Try submitting your result again."
+				);
+				return;
+			}
+		}
 
-            } catch (errorScreenshotResponses) {
-                message.reply("I didn't get a message with a screenshot. Try submitting your result again.");
-                return;
-            }
-            
-        }
+		// build result object
+		let result_obj = {
+			league: league,
+			coach: coach,
+			game_num: game_num,
+			images: images,
+		};
 
-        // build result object
-        let result_obj = {
-            "league": league,
-            "coach": coach,
-            "game_num": game_num,
-            "images": images
-        };
-        
-        // send result summary reply
-        // message.reply(
-        //     `Saved your result:\n` +
-        //     `**League:** ${league}\n` +
-        //     `**Coach:** ${coach}\n` +
-        //     `**Game:** ${game_num}\n` +
-        //     `**Score:** ${player_score}-${computer_score}\n`
-        // );
-        let resultSummaryEmbed = new discord.MessageEmbed()
-            .setColor(color_pending)
+		// send result summary reply
+		// message.reply(
+		//     `Saved your result:\n` +
+		//     `**League:** ${league}\n` +
+		//     `**Coach:** ${coach}\n` +
+		//     `**Game:** ${game_num}\n` +
+		//     `**Score:** ${player_score}-${computer_score}\n`
+		// );
+		let resultSummaryEmbed = new discord.MessageEmbed()
+			.setColor(color_pending)
 			.addFields(
 				{ name: "League", value: league, inline: true },
 				{ name: "Coach", value: coach, inline: true },
@@ -277,121 +295,121 @@ module.exports = {
 				{ name: "Score", value: `${player_score}-${computer_score}` }
 			)
 			.setImage(result_obj.images[0])
-            .setTimestamp();
-        
-        let resultSummaryMessage = await message.author.send(
-            "This is your result. React with ✅ if it looks right, or ❌ to cancel. Submitting in 15 seconds...",
-            { embed: resultSummaryEmbed });
-        
-        await resultSummaryMessage.react("✅");
-        await resultSummaryMessage.react("❌");
+			.setTimestamp();
 
-        const filter = (reaction, user) => {
-        
-            return (reaction.emoji.name === '✅' || reaction.emoji.name === '❌') && user.id === message.author.id;
-        };
+		let resultSummaryMessage = await message.author.send(
+			"This is your result. React with ✅ if it looks right, or ❌ to cancel. Submitting in 15 seconds...",
+			{ embed: resultSummaryEmbed }
+		);
 
-        let collected;
-        try {
-            collected = await resultSummaryMessage.awaitReactions(filter, { max: 1, time: 15000, errors: ['time'] });
-        } catch (errorCollected) {
-            collected = errorCollected;
-            collected.set('✅', {});
-            // resultSummaryMessage.reactions.removeAll();
-        }
-        
-        if (collected.has("✅")) {
-            // save to db
-            await resultSummaryMessage.edit("Sending...");
-            await saveResult(result_obj).catch((error) => {
-                throw error;
-            });
-            
-            await resultSummaryMessage.edit("Your result was recorded",
-                {
-                    embed: resultSummaryEmbed.setColor(color_recorded),
-                    reactions: {}
-                }
-            );
-            
-        }
-        else if (collected.has("❌")) {
-            await resultSummaryMessage.edit("This result was canceled", {
+		await resultSummaryMessage.react("✅");
+		await resultSummaryMessage.react("❌");
+
+		const filter = (reaction, user) => {
+			return (
+				(reaction.emoji.name === "✅" ||
+					reaction.emoji.name === "❌") &&
+				user.id === message.author.id
+			);
+		};
+
+		let collected;
+		try {
+			collected = await resultSummaryMessage.awaitReactions(filter, {
+				max: 1,
+				time: 15000,
+				errors: ["time"],
+			});
+		} catch (errorCollected) {
+			collected = errorCollected;
+			collected.set("✅", {});
+			// resultSummaryMessage.reactions.removeAll();
+		}
+
+		if (collected.has("✅")) {
+			// save to db
+			await resultSummaryMessage.edit("Sending...");
+			await saveResult(result_obj).catch((error) => {
+				throw error;
+			});
+
+			await resultSummaryMessage.edit("Your result was recorded", {
+				embed: resultSummaryEmbed.setColor(color_recorded),
+				reactions: {},
+			});
+		} else if (collected.has("❌")) {
+			await resultSummaryMessage.edit("This result was canceled", {
 				embed: resultSummaryEmbed.setColor(color_unrecorded),
 				reactions: {},
-            });
-            return;
-        }
-            
-        // check db for result pair
-        let result_pair_query = {
-            "league": league,
-            "coach": coach,
-            "game_num": game_num,
-        };
-      
-        let result_pair_obj = await getResultPair(result_pair_query);
+			});
+			return;
+		}
 
-        // if it's there, build and send a summary to the results channel
-        if (result_pair_obj != null) {
-            const channel = client.channels.cache.get(results_channel_id);
+		// check db for result pair
+		let result_pair_query = {
+			league: league,
+			coach: coach,
+			game_num: game_num,
+		};
 
-            // get additional game data from schedule
-            let schedule_data_query = {
-                "league": result_obj.league,
-                "coach": result_obj.coach,
-                "game_num": result_obj.game_num
-            };
-            let game_schedule_data = await getScheduleData(schedule_data_query)
-                .catch((error) => {
-                    throw error;
-                });
-            
-            if (game_schedule_data != null) {
-                let away_result_obj = game_schedule_data.away_role_id;
-                let home_result_obj = game_schedule_data.home_role_id;
+		let result_pair_obj = await getResultPair(result_pair_query);
 
-                if (result_obj.coach == game_schedule_data.away_coach_id) {
-                    away_result_obj = result_obj;
-                    home_result_obj = result_pair_obj;
-                } else {
-                    away_result_obj = result_pair_obj;
-                    home_result_obj = result_obj;
-                }
+		// if it's there, build and send a summary to the results channel
+		if (result_pair_obj != null) {
+			const channel = client.channels.cache.get(results_channel_id);
 
-                let awayMessage = 
+			// get additional game data from schedule
+			let schedule_data_query = {
+				league: result_obj.league,
+				coach: result_obj.coach,
+				game_num: result_obj.game_num,
+			};
+			let game_schedule_data = await getScheduleData(
+				schedule_data_query
+			).catch((error) => {
+				throw error;
+			});
+
+			if (game_schedule_data != null) {
+				let away_result_obj = game_schedule_data.away_role_id;
+				let home_result_obj = game_schedule_data.home_role_id;
+
+				if (result_obj.coach == game_schedule_data.away_coach_id) {
+					away_result_obj = result_obj;
+					home_result_obj = result_pair_obj;
+				} else {
+					away_result_obj = result_pair_obj;
+					home_result_obj = result_obj;
+				}
+
+				let awayMessage =
 					`**Game ${game_num} - ${game_schedule_data.away_role_id} vs. ${game_schedule_data.home_role_id}**\n` +
 					`Away: ${game_schedule_data.away_role_id}`;
-                
-                const awayObject = {
-                    "image": {
-                        "url": away_result_obj.images[0]
-                    },
-                    "timestamp": new Date().getTime()
-                };
-                let homeMessage = `Home: ${game_schedule_data.home_role_id}`;
-                const homeObject = {
-                    "image": {
-                        "url": home_result_obj.images[0]
-                    },
-                    "timestamp": new Date().getTime()
-                };
-                channel.send(awayMessage, {embed: awayObject}).then(() => {
-                    channel.send(homeMessage, {embed: homeObject});
-                });
-                
 
-            }
-            else {
-                throw Error(`Schedule data was null for this query: ${schedule_data_query}`);
-            }
-
-        }
-        // if it's not, do nothing
-        else {
-
-        }
-
-
-    }
-}
+				const awayObject = {
+					image: {
+						url: away_result_obj.images[0],
+					},
+					timestamp: new Date().getTime(),
+				};
+				let homeMessage = `Home: ${game_schedule_data.home_role_id}`;
+				const homeObject = {
+					image: {
+						url: home_result_obj.images[0],
+					},
+					timestamp: new Date().getTime(),
+				};
+				channel.send(awayMessage, { embed: awayObject }).then(() => {
+					channel.send(homeMessage, { embed: homeObject });
+				});
+			} else {
+				throw Error(
+					`Schedule data was null for this query: ${schedule_data_query}`
+				);
+			}
+		}
+		// if it's not, do nothing
+		else {
+		}
+	},
+};
--- commands/schedules.js
+++ commands/schedules.js
@@ -1,70 +1,160 @@
-const {Schedules} = require('../dbInit');
-const { Op } = require('sequelize')
+const { Schedules } = require("../dbInit");
+const { Op } = require("sequelize");
+const { discord } = require("../byb-bot");
 module.exports = {
-	name: 'schedules',
-	description: 'scheduling teams',
+	name: "schedules",
+	description: "scheduling teams",
 
 	async execute(message, args, client) {
+		if (
+			message.member.roles.cache.find(
+				(role) => role.name == "Commissioner"
+			)
+		) {
+			console.log("Is Commissioner");
+			try {
+				// equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
+				if (args.length == 6) {
+					const ss = await Schedules.create({
+						league: args[0],
+						game_num: args[1],
+						away_role_id: args[2],
+						away_coach_id: args[3],
+						home_role_id: args[4],
+						home_coach_id: args[5],
+						game_complete: false,
+					});
 
-	if (message.member.roles.cache.find(role => role.name == 'Commissioner')) {
-		console.log("Is Commisioner")
-					try {
-						// equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
-						if (args.length == 6){
-						const ss = await Schedules.create({
-		        	league:args[0],
-							game_num:args[1],
-		    			away_role_id:args[2],
-		    			away_coach_id:args[3],
-							home_role_id:args[4],
-							home_coach_id:args[5],
-							game_complete: false
-						});
+					// TEST DATA
+					// const ss = {
+					// 	league: args[0],
+					// 	game_num: args[1],
+					// 	away_role_id: args[2],
+					// 	away_coach_id: args[3],
+					// 	home_role_id: args[4],
+					// 	home_coach_id: args[5],
+					// 	game_complete: false,
+					// 	id: "test_id"
+					// };
 
+			
 					console.log("sending confirmation message");
+					let scheduleAddMessageEmbed = new discord.MessageEmbed()
+						.setDescription(`**${ss.league.toUpperCase()} Game ${ss.game_num}**
+						${ss.away_role_id} (${ss.away_coach_id}) at ${ss.home_role_id} (${ss.home_coach_id})`)
+						;
 
-					return message.reply('Added '+ss.league+ 'Game'+ ss.game_num + ss.away_role_id + ' at ' + ss.home_role_id);
+					return message.reply(`your schedule item was added *id# ${ss.id}*`, { embed: scheduleAddMessageEmbed });
 
-					}else if (args[0]=='delete' && args.length==2){
-						const deletion = await Schedules.destroy({ where: { id: parseInt(args[1]) } });
-						return message.reply('Scheduled Item Deleted')
+				} else if (args[0] == "delete" && args.length == 2) {
+					const deletion = await Schedules.destroy({
+						where: { id: parseInt(args[1]) },
+					});
+					return message.reply("Scheduled Item Deleted");
+				} else if (args[0] == "view") {
+					console.log("show schedule");
+					let view_games = await Schedules.findAll({
+						where: {
+							league: args[1],
+							game_num: args[2],
+						},
+					});
 
-					}else if (args[0]=='view'){
-								console.log("show schedule")
-								const view_games = await Schedules.findAll({
-									  where: {
-									    league: args[1],
-											game_num: args[2]
-									  }
-									});
-									let response="";
-									for(let i=0; i<view_games.length;i++){
-										if (view_games[i].game_complete){
-										response+=':white_check_mark:'
-									}else{
-										response+='       '
-									}
-										response+=view_games[i].away_role_id +' '+view_games[i].away_coach_id +' At '+view_games[i].home_role_id +' '+view_games[i].home_coach_id +' :id-> '+ view_games[i].id +'\n';
-									}
-
-									return message.channel.send(response);
-							}else{
-								console.log("sending Not confirmation message");
-								return message.reply('Please try the format of !schedules [LEAGUE] [GAME#] [AWAY TEAM ROLE] [AWAY TEAM INITAL] [HOME TEAM ROLE] [HOME TEAM INITIAL]');
-								return message.reply('Example ->  lulu 3 @Monsters BA @Twins MB');
-								//	}
-		//				}else{
-								}
-
+					// TEST DATA
+					// let view_games = [
+					// 	{
+					// 		game_num: 12,
+					// 		game_complete: false,
+					// 		away_role_id: "<@&735310865284333651>",
+					// 		away_coach_id: "JL",
+					// 		home_role_id: "<@&735310865284333651>",
+					// 		home_coach_id: "JL",
+					// 		id: "666",
+					// 	},
+					// 	{
+					// 		game_num: 11,
+					// 		game_complete: true,
+					// 		away_role_id: "<@&735310865284333651>",
+					// 		away_coach_id: "JL",
+					// 		home_role_id: "<@&735310865284333651>",
+					// 		home_coach_id: "JL",
+					// 		id: "667",
+					// 	},
+					// 	{
+					// 		game_num: 10,
+					// 		game_complete: false,
+					// 		away_role_id: "<@&735310865284333651>",
+					// 		away_coach_id: "JL",
+					// 		home_role_id: "<@&735310865284333651>",
+					// 		home_coach_id: "JL",
+					// 		id: "668",
+					// 	},
+					// ];
+					let response = "";
+					let scheduleViewEmbed = new discord.MessageEmbed()
+					if (view_games.length == 0) {
+						response = "There are no scheduled games for that league/week";
+						return message.reply(response);
 					}
-					catch (e) {
-							console.log(e);
-							return message.reply('Something went wrong with adding a game.');
-					}
 
-}else{
-	console.log("Not Commisioner");
-	return null;
-}
-}
+					if (args[2]) {
+						scheduleViewEmbed.setTitle(
+							`${args[1].toUpperCase()} Schedule - Game ${args[2]}`
+						);
+					}
+					else {
+						scheduleViewEmbed.setTitle(
+							`${args[1].toUpperCase()} Schedule`
+						);
+						view_games = view_games.sort((a, b) => parseInt(a.game_num) < parseInt(b.game_num) ? -1 : 1);
+					}
+					
+					let scheduleData = [];
+					for (let i = 0; i < view_games.length; i++) {
+						let scheduleItemText = "";
+						scheduleItemText += view_games[i].game_complete ? '✅' : '⭕️';
+						
+						scheduleItemText +=
+							`  **Game ${view_games[i].game_num}**` +
+							` ${view_games[i].away_role_id} (${view_games[i].away_coach_id})` +
+							` at ${view_games[i].home_role_id} (${view_games[i].home_coach_id}) *id#${view_games[i].id}*`;
+						scheduleData.push(scheduleItemText);
+						// response +=
+						// 	view_games[i].away_role_id +
+						// 	" " +
+						// 	view_games[i].away_coach_id +
+						// 	" At " +
+						// 	view_games[i].home_role_id +
+						// 	" " +
+						// 	view_games[i].home_coach_id +
+						// 	" :id-> " +
+						// 	view_games[i].id +
+						// 	"\n";
+					}
+					scheduleViewEmbed.setDescription(scheduleData.join("\n"));
+					scheduleViewEmbed.setFooter(`Requested by @${message.author.username}`);
+
+					return message.channel.send({embed: scheduleViewEmbed});
+				} else {
+					console.log("sending Not confirmation message");
+					return message.reply(
+						"Please try the format of !schedules [LEAGUE] [GAME#] [AWAY TEAM ROLE] [AWAY TEAM INITAL] [HOME TEAM ROLE] [HOME TEAM INITIAL]"
+					);
+					return message.reply(
+						"Example ->  lulu 3 @Monsters BA @Twins MB"
+					);
+					//	}
+					//				}else{
+				}
+			} catch (e) {
+				console.log(e);
+				return message.reply(
+					"Something went wrong with adding a game."
+				);
+			}
+		} else {
+			console.log("Not Commissioner");
+			return null;
+		}
+	},
 };
