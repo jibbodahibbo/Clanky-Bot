293eefd09cc2d43b9928b4636986f268e9253d58
jibbodahibbo <jtwesterkamp@gmail.com>
2021-04-09 23:53:22 -0500

    buying things!

--- commands/buy.js
+++ commands/buy.js
@@ -1,10 +1,10 @@
 const {ClankyCoins} = require('../dbInit');
 const allowed_channels = ['733773776357163079','741308777357377617','778266821006458950'];
-/*
+
 //General purchase agreement
 async function purchase(user, cost){
 	console.log("puchase attempted");
-	if (user.coins>=300){
+	if (user.coins>=cost){
 		try {
 			await ClankyCoins.decrement('coins', { by:cost, where: { username:user.username}});
 		} catch (e) {
@@ -17,7 +17,31 @@ async function purchase(user, cost){
 }
 
 //random gif selector.
-function congratsme(){
+function getJoke(user){
+	let jokes=["Why is Marky Dubois good in the outfield? || Because his frog is great at catching flies.||",
+	"Pete Wheeler kept wondered why the ball kept getting bigger and bigger. ||Then it hit him.||",
+	"Where does Tony Delveccio go when he needs a new uniform? ||New Jersey.||",
+	"What runs around a baseball field but never moves? ||A fence, but also Jose Meisenheimer||",
+	"On where does Dante Robinson eat his lunch? || Home Plate.||",
+	"Why couldn’t Kimmy Eckman get soda  pop at the double header? ||Because the home team lost the opener.||",
+	"Once Ronny Dobbs was pitching so bad, ||the crowd started singing Take Him Out of The Ball Game!||",
+	"Lisa Crockett saw a giant mouse so she tried hitting it with a baseball bat,|| now she has a lifetime ban from Disneyland||",
+	"What is Sally Dobbs favorite book? ||Artemis Fowl||",
+	"Jocinda Smith's nickname",
+	"What's Jorge Garcias favorite Movie? || Money Ball ||"
+	]
+
+	let rjokes=jokes[Math.floor(Math.random() * jokes.length)];
+	let e={
+	    "title": "I've got a good one "+user +"!",
+	    "color": 10038562,
+	    "description": rjokes
+	  }
+		return e;
+}
+
+//random gif selector.
+function congratsMe(user){
 	let gifs=[
 		'https://media0.giphy.com/media/13ggM8kgGhrxao/giphy.gif',
 		'https://media3.giphy.com/media/3PxTfzLmaquPgZf49V/200.gif',
@@ -38,40 +62,108 @@ function congratsme(){
 		'https://thumbs.gfycat.com/BreakableAdorableFritillarybutterfly.webp'
 	]
 
-	return gifs[Math.floor(Math.random() * gifs.length)];
+	let rgif=gifs[Math.floor(Math.random() * gifs.length)];
+	let e={
+	    "title": "Congratulations "+user +"!",
+	    "color": 10038562,
+	    "description": "You have redeemed a congratulatory gif for 300 CC.",
+	    "image": {
+	      "url":rgif
+	    }
+	  }
+		return e;
 }
-*/
+
+function insult(user,other){
+	let adjs=[
+	'Blue',
+	'Crazy',
+	'Green',
+	'Humongous',
+	'Junior',
+	'Little',
+	'Mighty',
+	'Red',
+	'Super-Duper',
+	'White',
+	];
+	let nouns=[
+		"Lug",
+		"Knucklhead",
+		"Lunkhead",
+		"Pud",
+		"Scab",
+		"Schmuck",
+		"Wacko",
+		"Yob",
+		"Fart Knocker",
+		"Dingus",
+		"Dweeb"
+	];
 
 
-module.exports = {
-	name: 'buy things',
-	description: 'buy some things from the bot with clankycoins',
-	async execute(message, args) {
-
-/*
-		if (!allowed_channels.includes(message.channel.id) && message.guild != null){
-			return null;
+let radj=adjs[Math.floor(Math.random() * adjs.length)];
+let rnoun=nouns[Math.floor(Math.random() * nouns.length)];
+let e={
+			"title": user+', you have redeemed an insult for 2,000 CC!',
+			"color": 10038562,
+			"description": '<@'+other+'> is a '+radj+' '+rnoun+'.',
 		}
-		//Attempt to find the user who is purchasing.
+	return e;
+}
+
+async function getPurchaser(p){
+//Attempt to find the user who is purchasing.
 		try {
-			const user = await ClankyCoins.findOne({ where: { username:message.author.username } });
+			const user = await ClankyCoins.findOne({ where: { username:p} });
+			return user;
 		}
 		//If they don't exist, let them know they don't exist yet and to type !clankycoins.
 		catch (e) {
 			console.log(e);
 			return message.reply('You may not be in the Clanky Coins Ledger, type !clankycoins to keep track of your coins.');
 			}
+}
+
+module.exports = {
+	name: 'buy',
+	description: 'buy some things from the bot with clankycoins',
+	async execute(message, args) {
+		console.log("hello");
+
+		let user=await getPurchaser(message.author.username);
+
 
 			if(args[0]=="congratsme"){
-				 if(await purchase(user.username, 300)){
-					 	return message.reply(congratsme());
+				 if(await purchase(user, 300)){
+					 	return message.reply({ embed:congratsMe(user.username)});
 				 }else{
 					 return message.reply('You only have '+ user.coins + ' Clanky Coins.')
 				 }
-
 			}
 
+			if(args[0]=="joke"){
+				 if(await purchase(user, 1000)){
+						return message.reply({ embed:getJoke(user.username)});
+				 }else{
+					 return message.reply('You only have '+ user.coins + ' Clanky Coins.')
+				 }
+			}
+
+			if(args[0]=="insult"){
+				 if(await purchase(user, 2000)){
+						return message.reply({ embed:insult(user.username,message.mentions.users.first())});
+				 }else{
+					 return message.reply('You only have '+ user.coins + ' Clanky Coins.')
+				 }
+			}
+
+			if(args[0]=="pennant"){
+				 if(await purchase(user, 10000)){
+						return message.reply("You have purchased a pennant for 10,000 CC! <@187776456519057409> will contact you shortly!");
+				 }else{
+					 return message.reply('You only have '+ user.coins + ' Clanky Coins.')
+				 }
+			}
 	}
-*/
-	},
 };
