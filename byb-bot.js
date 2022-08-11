0544a94b4f01da6057e700cb4144fe58706eb10f
jibbodahibbo <jtwesterkamp@gmail.com>
2022-08-11 16:28:41 -0500

    give up

--- byb-bot.js
+++ byb-bot.js
@@ -60,35 +60,35 @@ client.once('ready', () => {
 });
 
 // Override old message handler
+/*
+// delete Discord['Events.MessageCreate.handle'];
+//
+// Discord.Events.MessageCreate.handle = function(data){
+//
+//   const client = this.client;
+//   const channel = this.getChannel(data);
+//   if (channel) {
+//     if (!channel.isTextBased() || channel.messages==null) return {};
+//     console.log("NEW MC");
+//     const existing = channel.messages.cache.get(data.id);
+//     if (existing) return { message: existing };
+//     const message = channel.messages._add(data);
+//     channel.lastMessageId = data.id;
+//
+//     /**
+//      * Emitted whenever a message is created.
+//      * @event Client#messageCreate
+//      * @param {Message} message The created message
+//      */
+//     client.emit(Events.MessageCreate, message);
+//
+//     return { message };
+//   }
+//
+//   return {};
+// }
+// module.exports = Discord;
 
-
-delete Discord['Events.MessageCreate.handle'];
-
-Discord.Events.MessageCreate.handle = function(data){
-
-  const client = this.client;
-  const channel = this.getChannel(data);
-  if (channel) {
-    if (!channel.isTextBased() || channel.messages==null) return {};
-    console.log("NEW MC");
-    const existing = channel.messages.cache.get(data.id);
-    if (existing) return { message: existing };
-    const message = channel.messages._add(data);
-    channel.lastMessageId = data.id;
-
-    /**
-     * Emitted whenever a message is created.
-     * @event Client#messageCreate
-     * @param {Message} message The created message
-     */
-    client.emit(Events.MessageCreate, message);
-
-    return { message };
-  }
-
-  return {};
-}
-module.exports = Discord;
 //
 
 try {
