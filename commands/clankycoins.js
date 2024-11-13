87bda4928dff4c950c9afa6f2d15de0da6195c41
jibbodahibbo <jtwesterkamp@gmail.com>
2024-11-13 09:56:38 -0600

    ok

--- commands/clankycoins.js
+++ commands/clankycoins.js
@@ -1,51 +1,45 @@
-const {ClankyCoins} = require('../dbInit');
-const allowed_channels = ['733773776357163079','741308777357377617','778266821006458950'];
+const { ClankyCoins } = require('../dbInit');
+const allowed_channels = ['733773776357163079', '741308777357377617', '778266821006458950'];
 
-//test123123asdasd
-async function addUser(message){
+async function addUser(message) {
 	try {
 		const cc = await ClankyCoins.create({
 			user_id: message.author.id,
 			username: message.author.username,
-			tag:message.author.tag,
-			coins:0
+			tag: message.author.tag,
+			coins: 0,
 		});
-	}
-	catch (e) {
+		return cc;
+	} catch (e) {
 		if (e.name === 'SequelizeUniqueConstraintError') {
-			const user = await ClankyCoins.findOne({ where: { username: message.author.id} });
-			return message.reply(user.username + " has "+ user.coins + " clanky coins.");
-			}
+			const user = await ClankyCoins.findOne({ where: { user_id: message.author.id } });
+			return message.reply(`${user.username} has ${user.coins} clanky coins.`);
+		}
 		console.log(e);
 		return message.reply('Something went wrong with adding a user to the clanky coins ledger.');
-		}
-	return cc;
+	}
 }
 
-async function addUserByMention(message){
+async function addUserByMention(message) {
 	try {
-		// Create clankycoin usser in ledger.
+		const mentionedUser = message.mentions.users.first();
+		if (!mentionedUser) return message.reply("You need to mention a user.");
+
 		const cc = await ClankyCoins.create({
-			user_id: message.mentions.users.first().id,
-			username: message.mentions.users.first().username,
-			tag:message.mentions.users.first().tag,
-			coins:0
+			user_id: mentionedUser.id,
+			username: mentionedUser.username,
+			tag: mentionedUser.tag,
+			coins: 0,
 		});
-	}
-	catch (e) {
+
+		return message.reply(`${mentionedUser.tag} added to the Clanky Coin Ledger with 0 coins`);
+	} catch (e) {
 		if (e.name === 'SequelizeUniqueConstraintError') {
-			const user = await ClankyCoins.findOne({ where: { username: message.mentions.users.first().id } });
-			return message.reply(user.username + " has "+ user.coins + " clanky coins.");
+			const user = await ClankyCoins.findOne({ where: { user_id: message.mentions.users.first().id } });
+			return message.reply(`${user.username} has ${user.coins} clanky coins.`);
 		}
-			console.log(e);
-			return message.reply('Something went wrong with adding a user to the clanky coins ledger.');
-	}
-	const user = await ClankyCoins.findOne({ where: { username:message.mentions.users.first().id } });
-	if (!user){
-		return message.reply( message.mentions.users.first().tag + ' added to the Clanky Coin Ledger');
-	}else{
-		return message.reply( message.mentions.users.first().tag + ' has ' + user.coins + ' ClankyCoins');
-		//I refuse to distinguish between plural and singular here. don't @ me.
+		console.log(e);
+		return message.reply('Something went wrong with adding a user to the clanky coins ledger.');
 	}
 }
 
@@ -54,98 +48,67 @@ module.exports = {
 	description: 'Coins from clanky',
 
 	async execute(message, args) {
-
-		//Check for valid channel, or DM
-	/*	if (!allowed_channels.includes(message.channel.id) && message.guild != null){
-			console.log("incorrect channel");
-			return null;
-		}
-*/
-	if (message.member.roles.cache.find(role => role.name === 'Commissioner') || message.member.roles.cache.find(role => role.name === 'Codehead')) {
-		if (args[0]=="unbank"){
-			const deletion = await ClankyCoins.destroy({
-		    where: {user_id: args[1] },
-		  });
-          const deletion2 = await ClankyCoins.destroy({
-		    where: {username: args[1] },
-		  });
-		}
-	}
-
-
-		if (args[0]=="ledger"){
-				let msg="\n";
-				const ledger = await ClankyCoins.findAll();
-				for (var i = 0; i < ledger.length; i++) {
-					msg+=ledger[i].user_id +" "+ledger[i].username +":"+ ledger[i].coins+ "\n";
-				}
-				console.log(msg);
-				return message.reply(msg);
+		// Ensure the bot only responds in the allowed channels
+		if (!allowed_channels.includes(message.channel.id) && message.guild != null) {
+			return;
 		}
 
-		if (args.length<1){
-					let cc = await addUser(message);
-					return message.reply( cc.tag + ' added to the Clanky Coin Ledger');
+		// Check if the user has the required role
+		if (!message.member || (!message.member.roles.cache.some(role => ['Commissioner', 'Codehead'].includes(role.name)))) {
+			return message.reply('This is a commissioner-only command.');
+		}
+
+        if (args[0] === "ledger") {
+            let msg = "\n";
+            const ledger = await ClankyCoins.findAll();
+        
+            // Build the full ledger message
+            ledger.forEach(user => {
+                msg += `${user.user_id} ${user.username}: ${user.coins}\n`;
+            });
+        
+            // Split the message into chunks of up to 1900 characters
+            const messages = msg.match(/[\s\S]{1,1900}/g);
+        
+            // Send each chunk as a separate message
+            for (const chunk of messages) {
+                await message.reply(chunk);
+            }
+        }
+
+		// Handle adding a user
+		if (args.length < 1) {
+			const cc = await addUser(message);
+			return message.reply(`${cc.tag} added to the Clanky Coin Ledger`);
+		}
+
+		// Command: Add or remove coins from a mentioned user
+		if (message.mentions.users.size > 0 && args.length > 2) {
+			const mentionedUser = message.mentions.users.first();
+			const user = await ClankyCoins.findOne({ where: { user_id: mentionedUser.id } });
+			
+			if (user) {
+				const amount = parseInt(args[2], 10);
+				if (isNaN(amount)) return message.reply("Invalid amount specified.");
+
+				if (args[0] === "add") {
+					await ClankyCoins.increment('coins', { by: amount, where: { user_id: mentionedUser.id } });
+					return message.reply(`${mentionedUser.tag} now has a coin total of ${user.coins + amount}`);
 				}
 
-
-		if (message.member.roles.cache.find(role => role.name === 'Commissioner') || message.member.roles.cache.find(role => role.name === 'Codehead')) {
-			///Help Command
-			if (args.length==1){
-					if (args[0] == 'help'){
-						return message.reply('!clankycoins @user : !clankycoins add @user 100 : !clankycoins remove @user 100');
-
-			///Adding user by mention.
-					}else if (message.mentions.users.first()){
-						await addUserByMention(message);
+				if (args[0] === "remove") {
+					await ClankyCoins.decrement('coins', { by: amount, where: { user_id: mentionedUser.id } });
+					return message.reply(`${mentionedUser.tag} now has a coin total of ${user.coins - amount}`);
 				}
+			} else {
+				// Create a new user if they don't exist
+				await addUserByMention(message);
 			}
-
-			if (args.length>2){
-					const user = await ClankyCoins.findOne({ where: { username:message.mentions.users.first().id } });
-					if(user){
-					if (args[0]=="add"){
-						console.log("add");
-						await ClankyCoins.increment('coins', { by: parseInt(args[2]), where: { username: message.mentions.users.first().id } });
-						return message.reply(user.id + ' now has a coin total of ' + (user.coins + parseInt(args[2])) );
-					}
-					if (args[0]=="remove"){
-						console.log("remove");
-						await ClankyCoins.decrement('coins', { by: parseInt(args[2]), where: { username: message.mentions.users.first().id } });
-					  return message.reply(user.id + ' now has a coin total of ' +  (user.coins - parseInt(args[2])));
-					}
-				}
-				else{
-					if (message.mentions.users.first()){
-							try {
-								// Create clankycoin usser in ledger.
-								const cc = await ClankyCoins.create({
-									user_id: message.mentions.users.first().id,
-									username: message.mentions.users.first().id,
-									tag:message.mentions.users.first().tag,
-									coins:0
-								});
-								return message.reply( message.mentions.users.first().tag + ' added to the Clanky Coin Ledger with 0 coins');
-							}
-							catch (e) {
-								if (e.name === 'SequelizeUniqueConstraintError') {
-									let usertag=message.mentions.users.first().tag;
-									const user = await ClankyCoins.findOne({ where: { username: message.mentions.users.first().id } });
-									return message.reply(user.id + " has "+ user.coins + " clanky coins.");
-								}
-									console.log(e);
-									return message.reply('Something went wrong with adding a user to the clanky coins ledger.');
-								}
-					}
-
-				}
-				}
-
-			}else{
-				return message.reply('This is a commissioner only command.');
-			}
-
 		}
 
-
-,};
+		// Help Command
+		if (args[0] === "help") {
+			return message.reply('!clankycoins @user : !clankycoins add @user 100 : !clankycoins remove @user 100');
+		}
+	},
+};
