524430b1f9c3d11a9479ac13937bf93321e2540e
jibbodahibbo <jtwesterkamp@gmail.com>
2024-11-13 09:39:49 -0600

    temp02

---
+++ coaches_02.js
@@ -0,0 +1,196 @@
+module.exports = {
+	"data": {
+		"AX": {
+			"code": "AX",
+			"id": "272834771871334400",
+			"username": "alex7456",
+			"team_name": "Kansas Jayhawks",
+			"team_color": "",
+			"logo": ""
+		},
+		"AP": {
+			"code": "AP",
+			"id": "268060176320299019",
+			"username": "AnjouPays",
+			"team_name": "Evil Rays",
+			"team_color": "",
+			"logo": ""
+		},
+		"BO": {
+			"code": "BO",
+			"id": "894684630970671125",
+			"username": "bobbyJONES2370",
+			"team_name": "Texas Patriots",
+			"team_color": "",
+			"logo": ""
+		},
+		"C8": {
+			"code": "C8",
+			"id": "355931440061612035",
+			"username": "crazyei8hts",
+			"team_name": "Frequent Blimps",
+			"team_color": "",
+			"logo": ""
+		},
+		"CH": {
+			"code": "CH",
+			"id": "336232350088429589",
+			"username": "Chellow",
+			"team_name": "Rolling Cheesewheels",
+			"team_color": "",
+			"logo": ""
+		},
+		"PS": {
+			"code": "PS",
+			"id": "463930040036491264",
+			"username": "psanchez55",
+			"team_name": "Unintentional Cheese",
+			"team_color": "",
+			"logo": ""
+		},
+		"CW": {
+			"code": "CW",
+			"id": "430920494351515650",
+			"username": "MunchSmith",
+			"team_name": "Seattle Fishes",
+			"team_color": "",
+			"logo": ""
+		},
+		"HZ": {
+			"code": "HZ",
+			"id": "297541358502150144",
+			"username": "Heinz",
+			"team_name": "Dark Knights",
+			"team_color": "",
+			"logo": ""
+		},
+		"LO": {
+			"code": "LO",
+			"id": "196509313836122113",
+			"username": "Loba",
+			"team_name": "Florida Marlins",
+			"team_color": "",
+			"logo": ""
+		},
+		"LW": {
+			"code": "LW",
+			"id": "803449974800384040",
+			"username": "lillwaws26",
+			"team_name": "Los Angeles Brats",
+			"team_color": "",
+			"logo": ""
+		},
+		"M@": {
+			"code": "M@",
+			"id": "690369864618999849",
+			"username": "mattitude",
+			"team_name": "Jibbo's Ghosts",
+			"team_color": "",
+			"logo": ""
+		},
+		"WZ": {
+			"code": "WZ",
+			"id": "273653649522294784",
+			"username": "Wizard",
+			"team_name": "New York Slices",
+			"team_color": "",
+			"logo": ""
+		},
+		"YY": {
+			"code": "YY",
+			"id": "74323981670285312",
+			"username": "yuryavic",
+			"team_name": "Inky Squids",
+			"team_color": "",
+			"logo": ""
+		},
+		"IT": {
+			"code": "IT",
+			"id": "147462189404389376",
+			"username": "Itaniium",
+			"team_name": "Final Boss",
+			"team_color": "",
+			"logo": ""
+		},
+		"JY": {
+			"code": "JY",
+			"id": "307010267001257996",
+			"username": "jyknight",
+			"team_name": "West Michigan Whitecaps",
+			"team_color": "",
+			"logo": ""
+		},
+		"PZ": {
+			"code": "PZ",
+			"id": "394922258533187584",
+			"username": "personizzle",
+			"team_name": "Green Reds",
+			"team_color": "",
+			"logo": ""
+		},
+		"HC": {
+			"code": "HC",
+			"id": "738170256874405980",
+			"username": "hciii",
+			"team_name": "Crazy Beers",
+			"team_color": "",
+			"logo": ""
+		},
+		"2F": {
+			"code": "2F",
+			"id": "318576630819717120",
+			"username": "cantwait2fly",
+			"team_name": "San Francisco Giants",
+			"team_color": "",
+			"logo": ""
+		},
+		"N8": {
+			"code": "N8",
+			"id": "87804201056337920",
+			"username": "Natetastic28",
+			"team_name": "Harlem Shaking Grimaces",
+			"team_color": "",
+			"logo": ""
+		},
+		"BB": {
+			"code": "BB",
+			"id": "282618582574170114",
+			"username": "BBALL",
+			"team_name": "San Diego Padres",
+			"team_color": "",
+			"logo": ""
+		},
+		"TW": {
+			"code": "TW",
+			"id": "149700004208967681",
+			"username": "HaloTW",
+			"team_name": "Mightiest Melonheads",
+			"team_color": "",
+			"logo": ""
+		},
+		"VS": {
+			"code": "VS",
+			"id": "135436127862652928",
+			"username": "Vissery",
+			"team_name": "Freemly Qrxlzubs",
+			"team_color": "",
+			"logo": ""
+		},
+		"MF": {
+			"code": "MF",
+			"id": "424104533673902090",
+			"username": "scatty13",
+			"team_name": "Vaudeville Villains",
+			"team_color": "",
+			"logo": ""
+		},
+		"TO": {
+			"code": "TO",
+			"id": "692170461047554048",
+			"username": "Toast",
+			"team_name": "Minnesota Twins",
+			"team_color": "",
+			"logo": ""
+		}
+	}
+};
