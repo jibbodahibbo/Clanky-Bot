52be1490a00f02c4143737eb30203a3435f79356
jibbodahibbo <jtwesterkamp@gmail.com>
2025-05-14 16:02:08 -0500

    sponsor

---
+++ commands/sponsor.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'sponsor',
+	description: 'sponsor!',
+	execute(message) {
+		message.channel.send(`My sponsor for May and June 2025 is DEPaf679.
+			DEPaf679 is the coach of the Little Wombats of the Super-Duper League of the BBOL. 
+			In Season 5 DEPaf completed a stellar rookie season leading the Wombats to a 10-4 Record in the Little League.
+			In the rBBL DEPaf completed s13 in the 02 division 8-6 with the Aqua Teens earning a wildcard spot.
+			In the 03 division of the rBBL DEPaf took the Little Wombats to first place in the Frontyard Central and advanced all the way to the FYCS.`);
+	},
+};
